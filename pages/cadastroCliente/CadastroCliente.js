4c8df3080684db910813cb4bc72d58335a91daac
Zuma Elma Pereira <zumapereira@hotmail.com>
2023-09-06 18:40:59 -0300

    continua...

---
+++ pages/cadastroCliente/CadastroCliente.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { styles } from "./styles"
+
+function CadastroCliente() {
+
+
+    return(
+        <View style={styles.containerCadastroCliente}>
+            <View>
+                {/* aqui vai ser o topo */}
+            </View>
+            <View style={styles.body}>
+                {/* aqui vai ser o body */}
+            </View>
+            <View>
+
+            </View>
+        </View>
+    );
+
+}
+
+export default CadastroCliente
