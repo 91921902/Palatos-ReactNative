7fb7f7be0a719ddf7b34554eff4240cab824a4b3
João Pedro T <joaopedrot1912@gmail.com>
2023-09-06 13:53:39 -0300

    alteracoes

---
+++ pages/pagInicial.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function pagInicial() {
+
+
+    return(
+        <View>
+            
+        </View>
+    );
+
+}
+
+export default pagInicial
