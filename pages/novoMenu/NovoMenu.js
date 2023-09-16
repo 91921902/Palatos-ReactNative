c1963429d20f74a2891956d3aed11900382c146d
joão <joaopedrot1912@gmail.com>
2023-09-16 19:53:55 -0300

    base da tela novoMenu feito

--- pages/novoMenu/NovoMenu.js
+++ pages/novoMenu/NovoMenu.js
@@ -1,16 +1,88 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, {useState, useEffect} from "react";
+import { Text, TextInput, View, ScrollView, TouchableOpacity, Image } from "react-native";
 import { styles } from "./styles"
 import BotaoVoltar from "../../components/BotaoVoltar";
 import MiniLogo from "../../components/MiniLogo";
+import * as Font from 'expo-font';
+import fontKavoon from "../../assets/fonts/kavoon.ttf"
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 
 function NovoMenu() {
 
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    useEffect(() => {
+        async function loadFonts() {
+        await Font.loadAsync({
+            'kavoon': fontKavoon,
+        });
+        setFontLoaded(true);
+        }
+
+        loadFonts();
+    }, []);
+
+    if (!fontLoaded) {
+        return null; 
+    }
 
     return(
         <View style={styles.containerNovoMenu}>
             <BotaoVoltar />
             <MiniLogo />
+            <View style={styles.boxQuantMesas}>
+                <Text style={styles.textQuantMesas}>Quantidade de mesas do seu Restaurante:</Text>
+                <TextInput 
+                    keyboardType="numeric"
+                    style={styles.inputQuantMesas}
+                />
+            </View>
+            <View style={styles.titleMenu}>
+               <Text style={styles.textTitleMenu}>Faça seu Menu:</Text>
+            </View>
+            <ScrollView style={{width: "100%",height: "50%"}}>
+                <View style={styles.menuItens}>
+
+                    <View style={styles.item}>
+                        <View style={styles.boxPhoto}>
+                            <TouchableOpacity activeOpacity={1} style={styles.btnAddPhoto}>
+                                <View style={{position: "absolute", top: 2, right: 2}}>
+                                    <Icon 
+                                        name="plus"
+                                        size={25}
+                                        color={"#445A14"}
+                                        style={{alignItems: "flex-end"}}
+                                    />
+                                </View>
+                                <View style={{width: "100%", height: "100%", alignItems: "center", justifyContent: "center"}}>
+                                    <Icon 
+                                        name="cutlery"
+                                        size={30}
+                                        color={"#445A14"}
+                                    />
+                                 </View>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.boxDescItem}>
+
+
+
+                            
+                            <TouchableOpacity style={styles.btnAddItem}>
+                                <Image source={require("../../assets/icons/adicionar.png")} style={styles.imgAddItem}/>
+                            </TouchableOpacity>
+                        </View>    
+                    </View>
+
+                </View>
+            </ScrollView>
+            <View style={styles.boxFinalizarMenu}>
+                <TouchableOpacity style={styles.btnFinalizarMenu}>
+                    <Text style={styles.textFinalizarMenu}>Finalizar Menu</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 
--- pages/novoMenu/styles.js
+++ pages/novoMenu/styles.js
@@ -7,5 +7,105 @@ export const styles = StyleSheet.create({
         alignItems: "center",
         width: "100%",
         height: "100%"
+    },
+    boxQuantMesas: {
+        width: "100%",
+        height: "25%",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        gap: 20
+    },
+    inputQuantMesas: {
+        width: 100,
+        height: 40,
+        paddingLeft: 5,
+        backgroundColor: "#DAC8B3",
+        borderRadius: 2
+    },
+    textQuantMesas: {
+        fontSize: 15,
+        fontFamily: "kavoon",
+        width: "100%",
+        textAlign: "center",
+        color: "#445A14"
+    },
+    titleMenu: {
+        width: "100%",
+        height: "15%",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    textTitleMenu: {
+        fontSize: 25,
+        fontFamily: "kavoon",
+        width: "100%",
+        textAlign: "center",
+        color: "#445A14"
+    },
+    menuItens: {
+        alignItems: "center",
+        gap: 20,
+        paddingBottom: 20
+    },
+    boxFinalizarMenu: {
+        width: "100%",
+        height: "10%",
+        alignItems: "flex-end",
+        justifyContent: "center",
+        padding: 15
+    },
+    btnFinalizarMenu: {
+        width: 140,
+        height: 50,
+        backgroundColor: "#D1C0AB",
+        borderRadius: 2,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    textFinalizarMenu: {
+        fontSize: 15,
+        fontFamily: "kavoon",
+        width: "100%",
+        textAlign: "center",
+        color: "#445A14"
+    },
+    item: {
+        width: "90%",
+        height: 150,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    boxPhoto: {
+        height: 150,
+        width: 140,
+        backgroundColor: "#B7A187",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    btnAddPhoto: {
+        height: 130,
+        width: 120,
+        backgroundColor: "white",
+        borderRadius: 2,
+        position: "relative",
+    },
+    boxDescItem: {
+        flex: 1,
+        height: 150,
+        backgroundColor: "#D1C0AB",
+        position: "relative",
+        
+    },
+    btnAddItem: {
+        width: 30,
+        height: 30,
+        resizeMode: "contain",
+        position: "absolute",
+        bottom: -10,
+        right: -10,
+    },
+    imgAddItem: {
+        width: 30,
+        height: 30,
     }
 })
