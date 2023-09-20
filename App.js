5f7269dd9655e631b37afd764393be6c6b38e56a
João Pedro T <joaopedrot1912@gmail.com>
2023-09-20 16:47:15 -0300

    base visual da tela financeiro pronta

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import NovoCadastro from "./pages/novoCadastro/NovoCadastro.js"
 import NovoMenu from "./pages/novoMenu/NovoMenu.js"
 import PainelADM from './pages/painelADM/PainelADM.js';
+import Financeiro from './pages/financeiro/Financeiro.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <PainelADM />
+      <Financeiro />
     </View>
   );
 }
