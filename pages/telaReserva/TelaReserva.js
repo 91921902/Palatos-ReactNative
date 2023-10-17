f0ce30e604df46404d150111290a386480084f98
Dalison <dalisonmg@gmail.com>
2023-10-17 17:44:24 -0300

    Fazendo tela reserva

--- pages/telaReserva/TelaReserva.js
+++ pages/telaReserva/TelaReserva.js
@@ -2,3 +2,31 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 
+function TelaReserva() {
+    const [reservas, setReservas] = useState([])
+
+
+    useEffect(() => {
+        async function carregaReservas() {
+            {
+                //Precisa puxar do backend
+
+                //criando um objeto de data pra teste
+                const dataAtual = new Date()
+
+                const listaReservas = [
+                    {
+                        nome: "João",
+                        codigoReserva: 2524,
+                        horario: (dataAtual.getDate() + 1)
+                }
+                ]
+            }
+        }
+    })
+    return (
+
+    )
+}
+
+export default TelaReserva
