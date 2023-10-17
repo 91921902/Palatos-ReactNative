9d2bcfa4b190d4fb03a10b544c96961175640129
João Pedro T <joaopedrot1912@gmail.com>
2023-10-17 15:03:27 -0300

    Update styles.js

--- pages/filtros/styles.js
+++ pages/filtros/styles.js
@@ -5,7 +5,8 @@ export const styles = StyleSheet.create({
         width: "100%",
         height: "100%",
         alignItems: "center",
-        paddingBottom: 10
+        paddingBottom: 10,
+        backgroundColor: "#fff"
     },
     boxFilter: {
         width: "100%",
