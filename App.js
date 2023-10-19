36b13ee4a4dc34bed13b2bd8f92d5102b9bb28d3
Zuma Elma Pereira <132463705+91921902@users.noreply.github.com>
2023-10-19 14:36:11 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-// import CadastroCliente from './pages/cadastroCliente/CadastroCliente';
+import CadastroCliente from './pages/cadastroCliente/CadastroCliente';
 // import PagInicial from './pages/pagInicial/PagInicial';
 // import BuscaRestaurante from './pages/buscaRestaurante/BuscaRestaurante';
 // import PerfilRestaurante from './pages/perfilRestaurante/PerfilRestaurante';
-import MenuRestaurante from './pages/menuRestaurante/MenuRestaurante';
+// import MenuRestaurante from './pages/menuRestaurante/MenuRestaurante';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <CadastroCliente /> */}
+      <CadastroCliente />
       {/* <PagInicial /> */}
       {/* <BuscaRestaurante /> */}
       {/* <PerfilRestaurante />  */}
-      <MenuRestaurante />
+      {/* <MenuRestaurante /> */}
 
     </View>
   );
