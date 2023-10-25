52fde557333ddcea4f9eeb69128e5d8990229470
joão <joaopedrot1912@gmail.com>
2023-10-25 10:54:08 -0300

    criacao do componente

---
+++ components/Skeleton.js
@@ -0,0 +1,22 @@
+import {View, Text, StyleSheet} from "react-native"
+
+function Skeleton() {
+
+
+    return(
+        <View style={styles.container}>
+
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: "100%"
+    }
+
+
+})
+
+export default Skeleton
